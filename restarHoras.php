35d2d2a91e9023f38b1b9c2fce0186f37829d855
SaloValdez <salovaldez.net@gmail.com>
2018-07-25 10:32:08 -0500

    sumar  horas

---
+++ restarHoras.php
@@ -0,0 +1,30 @@
+<?php
+
+
+$var1  = '08:20:00';
+$var2  = '13:59:00';
+
+function RestarHoras($horaini,$horafin)
+{
+	$horai=substr($horaini,0,2);
+	$mini=substr($horaini,3,2);
+	$segi=substr($horaini,6,2);
+
+	$horaf=substr($horafin,0,2);
+	$minf=substr($horafin,3,2);
+	$segf=substr($horafin,6,2);
+
+	$ini=((($horai*60)*60)+($mini*60)+$segi);
+	$fin=((($horaf*60)*60)+($minf*60)+$segf);
+
+	$dif=$fin-$ini;
+
+	$difh=floor($dif/3600);
+	$difm=floor(($dif-($difh*3600))/60);
+	$difs=$dif-($difm*60)-($difh*3600);
+	return date("H-i-s",mktime($difh,$difm,$difs));
+}
+
+echo RestarHoras($var1,$var2);
+
+ ?>
